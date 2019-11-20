d472ad1b844aef3e5e3da09b12278d9c8c4416ab
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-11-19 21:30:42 -0600

    Add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// CONFIGURACIÓN
+
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d")
+
+canvas.width = 800;
+canvas.height = 600;
+
+// CLASES
+
+// INSTANCIAS 
+
+// FUNCIONES COMPLEMENTARIAS
+
+// FUNCIONES PRINCIPALES
+
+// EVENTOS
