e90faa78957e1593504352a11e450517f909b4be
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-11-21 19:10:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,81 @@
 // CONFIGURACIÓN
 
 let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d")
+let ctx = canvas.getContext("2d");
+
 
 canvas.width = 800;
 canvas.height = 600;
 
 // CLASES
 
-// INSTANCIAS 
+class Characters {
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw (){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Users extends Characters {
+    constructor(){
+        let x = canvas.width/2;
+        let y = canvas.height * 0.95;
+        let width = 20;
+        let height = 20;
+        super(x, y, width, height);
+        this.input = "";
+    }
+}
+
+class Asteroids extends Characters {
+    constructor(x,y){
+        let width = 20;
+        let height = 20;
+        super(x, y, width, height);
+    }
+
+    draw(){
+        this.A = 7;
+        this.B = 3;
+        this.answer = this.A + this.B;
+        ctx.font = "50px Arial";
+        ctx.fillText(`${this.A} + ${this.B} = ?`, this.x, this.y);
+    }
+} 
+
+// INSTANCIAS
+
+let player = new Users();
+player.draw();
+let asteroid = new Asteroids(50,50)
+asteroid.draw();
 
 // FUNCIONES COMPLEMENTARIAS
+
+function isValidKey(code){
+    return (code >= 48 && code <= 57) ||
+           (code == 13) || (code == 8);
+}
+
+function isEndOfLine(code){
+    return code == 13;
+}
 
 // FUNCIONES PRINCIPALES
 
 // EVENTOS
+
+document.onkeydown = (event) => {
+    if (isValidKey(event.keyCode)){
+        console.log(`The press key is: ${event.key}`);
+        while (!isEndOfLine(event.keyCode)){
+            
+        }
+    }
+}
